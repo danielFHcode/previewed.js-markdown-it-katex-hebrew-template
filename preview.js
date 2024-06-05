469f056bc408c23c1e26c69e39c29a5beca5910d
danfh <danfh9@gmail.com>
2024-06-05 17:46:40 +0300

    feat: added syntax highlighting with highlight.js

--- preview.js
+++ preview.js
@@ -2,6 +2,7 @@ import init from 'previewed';
 import markdown from '@previewed/plugin-markdown-it';
 import MarkdownIt from 'markdown-it';
 import katex from '@vscode/markdown-it-katex';
+import highlight from 'highlight.js';
 import css from '@previewed/plugin-css';
 import watch from '@previewed/plugin-watch';
 import { resolve } from 'node:path';
@@ -9,15 +10,25 @@ import { resolve } from 'node:path';
 init({
     dir: resolve('./src'),
     plugins: [
-        markdown(new MarkdownIt({ html: true }).use(katex.default)),
+        markdown(
+            new MarkdownIt({
+                html: true,
+                highlight(code, language) {
+                    return highlight.highlight(code, { language }).value;
+                },
+            }).use(katex.default)
+        ),
         css(
             `
             .katex-html {
                 display: none;
             }
-            *:not(.katex *) {
+            *:not(.katex *, pre, code, code *) {
                 font-family: david;
                 direction: rtl;
+            }
+            code, code *, pre {
+                direction: ltr;
             }
             [mathvariant=double-struck] {
                 text-shadow: 0.05em 0 white, 0.1em 0 black;
--- src/index.md
+++ src/index.md
@@ -1,3 +1,4 @@
 <link
   rel="stylesheet"
   href="https://fred-wang.github.io/MathFonts/LatinModern/mathfonts.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css">
